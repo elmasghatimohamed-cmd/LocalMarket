31f1e53d35c42996cd7a2026915f714bfcbe384a
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-02-03 23:09:51 +0100

    Creation des seeders pour les roles, permissions et utilisateurs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             ProductSeeder::class,
+            RolesAndPermissionsSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
