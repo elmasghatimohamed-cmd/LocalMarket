769887cc38be73afc79350539fc20913396add1d
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-02-04 16:31:47 +0100

    Modifications sur les routes et le seeder de la base de données

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::middleware([
 });
 
 
-Route::middleware(['auth', 'role:client'])->group(function () {
+Route::middleware(['auth', 'role:client|admin'])->group(function () {
     Route::get('/cart', [CartController::class, 'getCart'])->name('cart.index');
     Route::post('/cart/add', [CartController::class, 'addProduct'])->name('cart.add');
     Route::delete('/cart/remove/{id}', [CartController::class, 'removeProduct'])->name('cart.remove');
